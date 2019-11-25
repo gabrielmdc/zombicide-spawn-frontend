a9080960eb22bf188a3bb62dba03913ce7d9dd22
GMDC <gmdcwork@gmail.com>
2019-11-25 20:34:39 +0100

    first commit

---
+++ double-spawn-data.js
@@ -0,0 +1,34 @@
+const doubleSpawnData = [
+    {
+        "type": "DoubleSpawn",
+        "number": 54
+    },
+    {
+        "type": "DoubleSpawn",
+        "number": 53
+    },
+    {
+        "type": "DoubleSpawn",
+        "number": 52
+    },
+    {
+        "type": "DoubleSpawn",
+        "number": 51
+    },
+    {
+        "type": "DoubleSpawn",
+        "number": 50
+    },
+    {
+        "type": "DoubleSpawn",
+        "number": 49
+    },
+    {
+        "type": "DoubleSpawn",
+        "number": 48
+    },
+    {
+        "type": "DoubleSpawn",
+        "number": 47
+    }
+];
---
+++ necromancer-data.js
@@ -0,0 +1,26 @@
+const necromancerData = [
+    {
+        "type": "Necromancer",
+        "number": 41
+    },
+    {
+        "type": "Necromancer",
+        "number": 42
+    },
+    {
+        "type": "Necromancer",
+        "number": 43
+    },
+    {
+        "type": "Necromancer",
+        "number": 44
+    },
+    {
+        "type": "Necromancer",
+        "number": 45
+    },
+    {
+        "type": "Necromancer",
+        "number": 46
+    }
+];
